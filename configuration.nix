15fe88f810c5367aea640a9dc8fc45dff0c75554
ryndubei <114586905+ryndubei@users.noreply.github.com>
2024-06-04 22:03:33 +0100

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,175 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./virtualisation.nix
+      ./nvidia.nix
+    ];
+
+  # Nix features
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.initrd.luks.devices."luks-d7e8aab7-15a6-410b-b846-af3690149173".device = "/dev/disk/by-uuid/d7e8aab7-15a6-410b-b846-af3690149173";
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/London";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_GB.UTF-8";
+    LC_IDENTIFICATION = "en_GB.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+    LC_MONETARY = "en_GB.UTF-8";
+    LC_NAME = "en_GB.UTF-8";
+    LC_NUMERIC = "en_GB.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_TELEPHONE = "en_GB.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Remove certain Gnome packages
+  environment.gnome.excludePackages = (with pkgs.gnome; [
+    epiphany # web browser
+    geary # email reader
+  ]);
+
+  # Configure keymap in X11
+  services.xserver = {
+    xkb.layout = "gb";
+    xkb.variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "uk";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.vasilysterekhov = {
+    isNormalUser = true;
+    description = "Vasily Sterekhov";
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  # Misc services
+  services.flatpak.enable = true;
+  services.mullvad-vpn.enable = true;
+  services.mullvad-vpn.package = pkgs.mullvad-vpn;
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+  # Enable all sysrq functions (useful to recover from some issues):
+  boot.kernel.sysctl."kernel.sysrq" = 1; # NixOS default: 16 (only the sync command)
+  # Documentation: https://www.kernel.org/doc/html/latest/admin-guide/sysrq.html
+
+  # DNS-over-TLS
+  # https://docs.quad9.net/Setup_Guides/Linux_and_BSD/Ubuntu_22.04_%28Encrypted%29/
+  networking.nameservers = [ "9.9.9.9" "149.112.112.112" ];
+
+  services.resolved = {
+    enable = true;
+    dnssec = "false"; # "the DNSSEC option should not be enabled in systemd-resolved..."
+    domains = [ "~." ];
+    fallbackDns = [ "9.9.9.9" "149.112.112.112" ];
+    dnsovertls = "true";
+  };
+    
+  # haskell.nix and https://github.com/input-output-hk/devx
+  nix.settings.trusted-public-keys = [
+    "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
+  ];
+  nix.settings.substituters = [
+    "https://cache.iog.io"
+    "https://cache.zw3rk.com"
+  ];
+}
+
---
+++ virtualisation.nix
@@ -0,0 +1,143 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./nixos-vfio/modules/libvirtd/scopedHooks.nix
+    ];
+  
+  # set kernel params for virtualisation
+  boot.kernelParams = [ "intel_iommu=on" "iommu=pt" ];
+  boot.kernelModules = [ "kvm-intel" "vfio-pci" ];
+
+  systemd.services.libvirtd = { 
+    path = let 
+             env = pkgs.buildEnv {
+               name = "qemu-hook-env";
+               paths = with pkgs; [
+                 bash
+                 libvirt
+                 kmod
+                 systemd
+                 ripgrep
+                 sd
+               ];
+             };
+            in [ env ];
+    # note that you must place as21_patched.rom into /etc/nixos
+    preStart =
+    ''
+      mkdir -p /var/lib/libvirt/vgabios
+      ln -sf /etc/nixos/as21_patched.rom /var/lib/libvirt/vgabios/as21_patched.rom
+    '';
+  };  
+
+  virtualisation.libvirtd = {
+    enable = true;
+    onBoot = "ignore";
+    onShutdown = "shutdown";
+    qemu.runAsRoot = true;
+    qemu.ovmf.enable = true;
+  };
+  programs.virt-manager.enable = true;
+  
+  virtualisation.libvirtd.scopedHooks.qemu = {
+
+    start = {
+      enable = true;
+
+      scope = {
+        objects = [ "win10" ];
+        operations = [ "prepare" ];
+        subOperations = [ "begin" ];
+      };
+
+      script = ''
+        set -x
+        
+        # Stop display manager
+        systemctl stop display-manager
+        # rc-service xdm stop
+            
+        # Unbind VTconsoles: might not be needed
+        echo 0 > /sys/class/vtconsole/vtcon0/bind
+        echo 0 > /sys/class/vtconsole/vtcon1/bind
+        
+        # Unbind framebuffer
+        echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
+        
+        # Unload NVIDIA kernel modules
+        modprobe -r nvidia_drm nvidia_modeset nvidia_uvm nvidia
+        
+        # Unload AMD kernel module
+        # modprobe -r amdgpu
+        
+        # Detach GPU devices from host
+        # Use your GPU and HDMI Audio PCI host device
+        virsh nodedev-detach pci_0000_01_00_0
+        virsh nodedev-detach pci_0000_01_00_1
+        
+        # Load vfio module
+        modprobe vfio-pci
+      '';
+    };
+
+    # using this as an exit hook instead because 'stop' causes a black screen
+    rebootHost = {
+      enable = true;
+
+      scope = {
+        objects = [ "win10" ];
+        operations = [ "release" ];
+        subOperations = [ "end" ];
+      };
+
+      script = ''
+        reboot
+      '';
+    };
+
+    #stop = {
+    #  enable = true;
+
+    #  scope = {
+    #    objects = [ "win10" ];
+    #    operations = [ "release" ];
+    #    subOperations = [ "end" ];
+    #  };
+
+    #  script = ''
+    #    set -x
+    #    
+    #    # Unload vfio module
+    #    modprobe -r vfio-pci
+    #    
+    #    # Attach GPU devices to host
+    #    # Use your GPU and HDMI Audio PCI host device
+    #    virsh nodedev-reattach pci_0000_01_00_0
+    #    virsh nodedev-reattach pci_0000_01_00_1
+
+    #    # Load NVIDIA kernel modules
+    #    modprobe nvidia_drm
+    #    modprobe nvidia_modeset
+    #    modprobe nvidia_uvm
+    #    modprobe nvidia
+    #    
+    #    # Rebind framebuffer to host
+    #    echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/bind
+
+    #    # Load AMD kernel module
+    #    # modprobe amdgpu
+    #        
+    #    # Bind VTconsoles: might not be needed
+    #    echo 1 > /sys/class/vtconsole/vtcon0/bind
+    #    echo 1 > /sys/class/vtconsole/vtcon1/bind
+    #    
+    #    # Restart Display Manager
+    #    systemctl start display-manager
+    #    # rc-service xdm start
+    #  '';
+    #};
+  };
+}
+
