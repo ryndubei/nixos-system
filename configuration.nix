35c9dab1878a212dad0b4d602a85401798de32c5
ryndubei <114586905+ryndubei@users.noreply.github.com>
2024-06-22 15:00:24 +0100

    Enable Git system-wide

--- configuration.nix
+++ configuration.nix
@@ -138,6 +138,9 @@ in
     cachix
   ];
 
+  # Install Git
+  programs.git.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
