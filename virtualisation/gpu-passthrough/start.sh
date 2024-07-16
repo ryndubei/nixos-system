41eb8d26f80979c71627a3ffaed931d53a2e8483
ryndubei <114586905+ryndubei@users.noreply.github.com>
2024-07-16 18:26:26 +0100

    Remove unnecessary lines in vm hooks

--- virtualisation/gpu-passthrough/start.sh
+++ virtualisation/gpu-passthrough/start.sh
@@ -4,21 +4,11 @@ set -x
 # Stop display manager
 systemctl stop display-manager.service
 
-# Unbind EFI-Framebuffer
-echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
-
-# Avoid a Race condition by waiting 2 seconds. This can be calibrated to be shorter or longer if required for your system
-sleep 2
-
 # Unload all Nvidia drivers
 modprobe -r nvidia_drm
 modprobe -r nvidia_modeset
 modprobe -r nvidia_uvm
 modprobe -r nvidia
-
-# Detach GPU devices from host
-virsh nodedev-detach $VIRSH_GPU_VIDEO
-virsh nodedev-detach $VIRSH_GPU_AUDIO
 
 ## Load vfio
 modprobe vfio
