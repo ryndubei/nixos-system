d2cff06f4ca998fa8b928753b91e4bde8ebc997d
ryndubei <114586905+ryndubei@users.noreply.github.com>
2024-08-01 10:42:10 +0100

    Disable CUPS for printing

--- configuration.nix
+++ configuration.nix
@@ -85,9 +85,6 @@ in
   # Configure console keymap
   console.keyMap = "uk";
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
