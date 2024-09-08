e67d071b6ec4e0e05213296268e586e8a1501513
ryndubei <114586905+ryndubei@users.noreply.github.com>
2024-09-08 23:30:46 +0100

    Revert "Don't pass through the wifi card"
    This reverts commit 27d10adfd9d163aa6daddd118f533bfefc9d207f.

--- virtualisation/win10.nix
+++ virtualisation/win10.nix
@@ -22,6 +22,7 @@ let
   gpu-video = { domain = 0; bus = 1; slot = 0; function = 0; };
   gpu-audio = { domain = 0; bus = 1; slot = 0; function = 1; };
   nvme-ssd = { domain = 0; bus = 2; slot = 0; function = 0; };
+  wifi-controller = { domain = 0; bus = 4; slot = 0; function = 0; };
 
   # - Helper functions
 
@@ -214,6 +215,9 @@ in
               { source-address = gpu-video; rom-file = gpu-passthrough/as21_patched.rom; }
               { source-address = gpu-audio; rom-file = gpu-passthrough/as21_patched.rom; }
 
+              # Passing wifi-controller through here as we normally do not want
+              # to pass it through when running win10-nogpu
+              { source-address = wifi-controller; }
             ]);
 
           input = [
@@ -234,6 +238,7 @@ in
           channel = builtins.filter (x: x.type != "spiceport") old-xml.devices.channel;
           video.model.type = "none";
           redirdev = null;
+          interface = null; # since we are already passing through the network card
         };
 
         # Use /dev/kvmfr0 as shared memory for looking-glass
