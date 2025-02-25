312693a07435fab73e7fd5a78d158c27b12642b4
ryndubei <114586905+ryndubei@users.noreply.github.com>
2025-02-25 13:12:15 +0000

    Revert "Clean up extra substituters"
    This reverts commit bbd06e5bd5a8c42ce1457ebfab6612cc6c7aadd2.

--- configuration.nix
+++ configuration.nix
@@ -209,6 +209,8 @@ in {
 
   # haskell.nix and https://github.com/input-output-hk/devx
   nix.settings.trusted-public-keys = [
+    "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
     "ghc-nix.cachix.org-1:wI8l3tirheIpjRnr2OZh6YXXNdK2fVQeOI4SVz/X8nA="
     "haskell-language-server.cachix.org-1:juFfHrwkOxqIOZShtC4YC1uT1bBcq2RSvC7OMKx0Nz8="
     "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo="
@@ -216,6 +218,8 @@ in {
   ];
   nix.settings.substituters = lib.mkAfter [
     "https://ros.cachix.org"
+    "https://cache.iog.io?priority=999"
+    "https://cache.zw3rk.com?priority=1000"
     "https://ghc-nix.cachix.org"
     "https://haskell-language-server.cachix.org"
     "https://nix-community.cachix.org"
