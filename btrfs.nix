37121d50bd2e1fdbde40447ca568ff0f64ff34f2
ryndubei <114586905+ryndubei@users.noreply.github.com>
2025-08-23 06:24:55 +0100

    Set up flat subvolumes

--- btrfs.nix
+++ btrfs.nix
@@ -1,10 +1,24 @@
-{
-  services.btrfs.autoScrub.enable = true;
-
-  # TODO: flat subvolume structure
-  fileSystems."/".options = [
+let
+  subvolumeOptions = [
     "compress-force=zstd:5"
     "noatime"
     "user_subvol_rm_allowed" # safe: users can already delete their own subvolumes with rm
   ];
+  mkSubvol = name: {
+    device = "/dev/mapper/cryptroot";
+    fsType = "btrfs";
+    options = [ "subvol=${name}" ] ++ subvolumeOptions;
+  };
+in {
+  services.btrfs.autoScrub.enable = true;
+
+  fileSystems."/".options = subvolumeOptions;
+
+  fileSystems = {
+    "/home" = mkSubvol "@home";
+    "/srv" = mkSubvol "@srv";
+    "/nix" = mkSubvol "@nix";
+    "/tmp" = mkSubvol "@tmp";
+    "/var/tmp" = mkSubvol "@var_tmp";
+  };
 }
