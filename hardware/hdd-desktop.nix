9b55f45ed2e48595ee5779c164f069a4c9a75eb2
ryndubei <114586905+ryndubei@users.noreply.github.com>
2026-02-04 21:44:24 +0000

    Use bcachefs for HDD

--- hardware/hdd-desktop.nix
+++ hardware/hdd-desktop.nix
@@ -1,23 +1,39 @@
+{ pkgs, ... }:
+
 {
-  # Currently, if a disk is entered in boot.initrd.luks and missing,
-  # we will fail to boot. So they are in crypttab instead.
-  environment.etc.crypttab.text = ''
-    # SMR drive, so discards are necessary
-    crypthdd UUID=aeff52a8-1998-4bc7-bb81-ed93b6eaa652 /etc/secrets/keyfiles/smr_hdd_keyfile.key discard,nofail
-  '';
+  boot.supportedFilesystems = [ "bcachefs" ];
 
-  fileSystems."/mnt/hard_drive" = {
-    device = "/dev/mapper/crypthdd";
-    fsType = "ext4";
-    options = [ "x-gvfs-show" "nofail" ];
+  # https://wiki.nixos.org/wiki/Bcachefs
+  systemd.services."bcachefs-mount" = {
+    after = [ "local-fs.target" ];
+    wantedBy = [ "multi-user.target" ];
+    environment = {
+      MOUNT_POINT = "/mnt/hard_drive";
+      FS_UUID = "e254ac8f-25ca-434b-bef2-039becdaff41";
+    };
+    script = ''
+      #!${pkgs.runtimeShell} -e
+
+      ${pkgs.keyutils}/bin/keyctl link @u @s
+
+      # Check if the drive is already mounted
+      if ${pkgs.util-linux}/bin/mountpoint -q "$MOUNT_POINT"; then
+        echo "Drive already mounted at $MOUNT_POINT. Skipping..."
+        exit 0
+      fi
+
+      # Wait for the device to become available
+      while [ ! -b "/dev/disk/by-uuid/$FS_UUID" ]; do
+        echo "Waiting for $FS_UUID to become available..."
+        sleep 5
+      done
+
+      # Mount the device
+      ${pkgs.bcachefs-tools}/bin/bcachefs mount -o x-gvfs-show -f /etc/secrets/keyfiles/smr_hdd_keyfile.key UUID="$FS_UUID" "$MOUNT_POINT"
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
   };
-
-  boot.initrd.kernelModules = [ "dm-cache-default" ];
-
-  # Only enabled for the HDD. Root does not need it because root
-  # is on BTRFS with discard=async
-  services.fstrim.enable = true;
-
-  # cache_check warning
-  services.lvm.boot.thin.enable = true;
 }
