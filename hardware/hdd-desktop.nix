5ea328262706ff8d04dc49ad16c9bb249141a5d6
ryndubei <114586905+ryndubei@users.noreply.github.com>
2026-02-04 22:19:49 +0000

    Add ext4 hdd partition

--- hardware/hdd-desktop.nix
+++ hardware/hdd-desktop.nix
@@ -3,6 +3,18 @@
 {
   boot.supportedFilesystems = [ "bcachefs" ];
 
+  services.fstrim.enable = true;
+
+  environment.etc.crypttab.text = ''
+    cryptext4_hdd UUID=a2ae70d7-145c-455e-964a-43e6802080c6 /etc/secrets/keyfiles/smr_hdd_ext4_keyfile.key discard,nofail
+  '';
+
+  fileSystems."/mnt/ext4_hdd" = {
+    device = "/dev/mapper/cryptext4_hdd";
+    fsType = "ext4";
+    options = [ "x-gvfs-show" "nofail" ];
+  };
+
   # https://wiki.nixos.org/wiki/Bcachefs
   systemd.services."bcachefs-mount" = {
     after = [ "local-fs.target" ];
