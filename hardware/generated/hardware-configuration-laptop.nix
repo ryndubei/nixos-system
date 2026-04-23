fff373393f11464d34315b92dab195d2c61b2e5a
ryndubei <114586905+ryndubei@users.noreply.github.com>
2026-04-23 17:44:31 +0100

    Change /boot fmask

--- hardware/generated/hardware-configuration-laptop.nix
+++ hardware/generated/hardware-configuration-laptop.nix
@@ -24,7 +24,7 @@
   fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/6367-DC39";
       fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
+      options = [ "fmask=0077" "dmask=0022" ];
     };
 
   swapDevices = [ ];
